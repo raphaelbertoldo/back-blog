b63a6059d9dfc7de5f922e200f42096b595c1269
Raphael <rxphxel7k@gmail.com>
2022-12-26 09:40:02 -0300

    file upload

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,9 +1,16 @@
+const { existsSync, mkdirSync } = require("fs");
 const multer = require("multer");
+const { join } = require("path");
 
 const path = require("path");
 
 exports.storage = multer.diskStorage({
   destination: (req, res, callback) => {
+    const uploadDir = join(__dirname, "..", "..", "public");
+    if (!existsSync(uploadDir)) {
+      mkdirSync(uploadDir, { recursive: true });
+    }
+
     callback(null, path.resolve("public"));
   },
   filename: (req, file, callback) => {
